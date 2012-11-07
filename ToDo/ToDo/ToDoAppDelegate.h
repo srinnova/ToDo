4dc6c026d6e76214067617ce1bd098eb983169f7
srinnova <spasija.radoeshka@innovaworks.net>
2012-11-07 12:45:36 +0100

    first commit

---
+++ ToDo/ToDo/ToDoAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ToDoAppDelegate.h
+//  ToDo
+//
+//  Created by Mit on 11/7/12.
+//  Copyright (c) 2012 Innovaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ToDo/ToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Mit on 11/7/12.
+//  Copyright (c) 2012 Innovaworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ToDoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ToDoAppDelegate class]));
+    }
+}
